115dd5577339cbbf7ab807e2d199d9e399e18a32
i6319956 <v.cauwegoeres@student.maastrichtuniversity.nl>
2024-04-15 18:21:00 +0200

    Google Java Style changes

--- src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -9,33 +9,40 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class fileOutputStrategy implements OutputStrategy {
 
-    private String BaseDirectory;
+    // Changed variable name to camelCase
+    private String baseDirectory;
 
-    public final ConcurrentHashMap<String, String> file_map = new ConcurrentHashMap<>();
+    //Changed map to UPPER_SNAKE_CASE
+    public final ConcurrentHashMap<String, String> FILE_MAP = new ConcurrentHashMap<>();
 
     public fileOutputStrategy(String baseDirectory) {
 
-        this.BaseDirectory = baseDirectory;
+        // Changed variable name to camelCase
+        this.baseDirectory = baseDirectory;
     }
 
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         try {
             // Create the directory
-            Files.createDirectories(Paths.get(BaseDirectory));
+            // Changed variable name to camelCase
+            Files.createDirectories(Paths.get(baseDirectory));
         } catch (IOException e) {
             System.err.println("Error creating base directory: " + e.getMessage());
             return;
         }
-        // Set the FilePath variable
-        String FilePath = file_map.computeIfAbsent(label, k -> Paths.get(BaseDirectory, label + ".txt").toString());
+        // Set the filePath variable
+        // Changed baseDirectory name to camelCase
+        //Changed filePath name to camelCase
+        //Changed map to UPPER_SNAKE_CASE
+        String filePath = FILE_MAP.computeIfAbsent(label, k -> Paths.get(baseDirectory, label + ".txt").toString());
 
         // Write the data to the file
         try (PrintWriter out = new PrintWriter(
-                Files.newBufferedWriter(Paths.get(FilePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
+                Files.newBufferedWriter(Paths.get(filePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) { // Changed variable name to camelCase
             out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
         } catch (Exception e) {
-            System.err.println("Error writing to file " + FilePath + ": " + e.getMessage());
+            System.err.println("Error writing to file " + filePath + ": " + e.getMessage()); // Changed variable name to camelCase
         }
     }
 }
