72c3a7823e4d6cc6affe0e17c6bcf039581cf65e
i6319956 <v.cauwegoeres@student.maastrichtuniversity.nl>
2024-04-16 13:56:03 +0200

    Added Javadoc Documentation

--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,21 @@
 package com.cardio_generator.outputs;
 
+/**
+ * Defines a strategy for outputting patient health data. This interface
+ * should be implemented by classes that manage the output of data to different
+ * destinations like consoles, files, web sockets, or TCP connections.
+ */
 public interface OutputStrategy {
+
+    /**
+     * Outputs health data for a given patient. Implementations of this method
+     * are responsible for formatting and transmitting the data to the appropriate
+     * output destination specified by the concrete class (e.g., console, file, web).
+     * 
+     * @param patientId the unique identifier of the patient
+     * @param timestamp the timestamp of the data generation, representing when the data was recorded or generated
+     * @param label a label describing the type of data (e.g., "ECG", "Blood Pressure")
+     * @param data the actual health data as a string, which could include measurements or alerts
+     */
     void output(int patientId, long timestamp, String label, String data);
 }
